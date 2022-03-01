19083093e42fa0bc35cbadb3c123f0c57ed5f082
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-03-01 20:40:12 +0600

    modal, other and sencor detailes are displayed

--- js/script.js
+++ js/script.js
@@ -1,26 +1,22 @@
-//Error style
-const emptyError = document.getElementById('empty-error').style.display = 'none';
-//details box hiding stytle
-const detailsBox = document.getElementById('full-details').style.display = 'none';
-//Spinners style
-const spinners = document.getElementById('spinner-box').style.display = 'none';
-
 
 
 const searchBtn = () =>{
     
-    
     const searchText = document.getElementById('search-fild').value.toLowerCase();
-    if(searchText == ''){
-        const emptyError = document.getElementById('empty-error').style.display = 'block';
+     
+    if(searchText === '' ){
+        document.getElementById('empty-error').style.display = 'block';
     }
     else{
-        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+            document.getElementById('empty-error').style.display = 'block';
+       
+            const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
         fetch(url)
         .then(res => res.json())
         .then(data => showPhonesData(data.data));
-        const emptyError = document.getElementById('empty-error').style.display = 'none';
-        const spinners = document.getElementById('spinner-box').style.display = 'block';
+        document.getElementById('empty-error').style.display = 'none';
+        document.getElementById('spinner-box').style.display = 'block';
+        
     }
 }
 
@@ -37,7 +33,7 @@ const showPhonesData = datas =>{
                 <div class="card-body">
                 <h5 class="card-title">Brand: ${data.brand}</h5>
                 <h5 class="card-title">Name: ${data.phone_name}</h5>
-                <button onclick="fullDetails('${data.slug}')" type="button" class="btn btn-primary">Full Details</button>
+                <button onclick="fullDetails('${data.slug}')" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Full Details</button>
                 </div>
             </div>
         `;
@@ -47,23 +43,62 @@ const showPhonesData = datas =>{
 }
 
 
-//full details showing section
-const fullDetails = phoneInfo =>{
-    const url =` https://openapi.programming-hero.com/api/phone/${phoneInfo}`
+//getting full details data
+const fullDetails = getPhoneInfo =>{
+    const url =` https://openapi.programming-hero.com/api/phone/${getPhoneInfo}`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data.data.brand))
-
+    .then(data => showingFullData(data.data))
 
     // const detailsBox = document.getElementById('full-details').style.display = 'block';
+}
+//showing full details 
+const showingFullData = showPhoneInfo =>{
+    
+        const {sensors} = showPhoneInfo.mainFeatures;
+        
+        const {name, image, releaseDate, others} = showPhoneInfo;
 
+        const modalImg = document.getElementById('modal-img');
+         modalImg.setAttribute('src', `${image}`);
 
+         const modalDetails = document.getElementById('modal-details');
+
+        modalDetails.textContent = '';
+        
+         
+        modalDetails.innerHTML = `
+        
+            <h3 class="mt-2 text-center">Name: ${name}</h3> 
+            <h5 class="mt-2 text-center">Release Date: ${releaseDate ? releaseDate : 'Not Found'}</h5>
+            <div>Sensors:<p id="sensor-details"></p></div>
+            <div><b>Other Details:</b> <p id="others-details"></p></div>
+
+        `;
+      
+        for(const sensor of sensors){
+            const sensorId = document.getElementById('sensor-details');
+            const p = document.createElement('p');
+            p.innerText = `Sensors name: ${sensor}`;
+            sensorId.appendChild(p);
+        }
+        for (const other in others){
+             const otherDetails = document.getElementById('others-details');
+             const p = document.createElement('p');
+             p.innerText = `
+                    Others Details: ${other};
+             `;
+             otherDetails.appendChild(p);
+        }
+
+       
+
+        
+        
 }
 
-//Full details closing button section
-const closeByX = () =>{
-    const detailsBox = document.getElementById('full-details').style.display = 'none';
-}
-const closeByBtn = () => {
-    const detailsBox = document.getElementById('full-details').style.display = 'none';
-}
+
+
+
+
+
